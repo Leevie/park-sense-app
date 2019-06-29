999a245a1e93edcf7622af7bea2358530d7fa160
Smarty Pants <amcurrent@hotmail.com>
2019-06-29 12:04:54 -0500

    preliminary styling for jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -6,9 +6,9 @@ function Jumbotron({ type = "default", className,onClick }) {
     <div className="jumbotron text-center">
       <h1>Park Rangers</h1>
       <p>Review local parks in your area</p>
-      <button onClick={onClick} className="btn btn-lg">See parks by age</button>
-      <button onClick={onClick} className="btn btn-lg">See parks by area</button>
-      <button onClick={onClick} className="btn btn-lg">See parks by popularity</button>
+      <button onClick={onClick} className="btn btn-lg button mr-3">See parks by age</button>
+      <button onClick={onClick} className="btn btn-lg button mr-3">See parks by area</button>
+      <button onClick={onClick} className="btn btn-lg button">See parks by popularity</button>
     </div>
   );
 }
