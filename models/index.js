6ffd3fb8f3cbf7b9726b7987471ecf8c7aead8b9
Leevie Martin <leevie@gmail.com>
2019-07-03 22:38:27 -0500

    Re-cloned the Master

--- models/index.js
+++
@@ -1,9 +0,0 @@
-// Exporting an object containing all of our models
-
-module.exports = {
-    Park: require("./park"),
-    ParkFeatures: require("./parkfeatures"),
-    ParkFeaturesReviws: require("./parkfeaturesreviews"),
-    ParkReviews: require("./parkreviews"),
-    User: require("./user")
-};
