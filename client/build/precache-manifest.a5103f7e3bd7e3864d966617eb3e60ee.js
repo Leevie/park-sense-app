faf002903758360e23a0cbd23f4f9ecb11f29624
Leevie Martin <leevie@gmail.com>
2019-07-03 20:03:08 -0500

    Fix .gitignore to allow build folder, nav bar fix

---
+++ client/build/precache-manifest.a5103f7e3bd7e3864d966617eb3e60ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186fd66a861d5d15653b44614be08a2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc45819f4758294a46d7",
+    "url": "/static/css/main.2c39ac4a.chunk.css"
+  },
+  {
+    "revision": "c62c45abac34ce1d66dc",
+    "url": "/static/js/2.754fa45f.chunk.js"
+  },
+  {
+    "revision": "bc45819f4758294a46d7",
+    "url": "/static/js/main.9f6f90b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
