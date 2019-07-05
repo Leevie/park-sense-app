49930d44ac678d12100cd21d02e28c669b7a28d2
Smarty Pants <amcurrent@hotmail.com>
2019-07-05 14:02:10 -0500

    changed styling and  titleto 'Parkify'

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,8 +5,8 @@ import "./style.css";
 function Jumbotron({ type = "default", className,onClick }) {
   return (
     <div className="jumbotron text-center">
-       <h1 style={{fontSize: '80px', color: 'white'}}>Park Rangers</h1> 
-        <p style={{fontSize: '40px', fontWeight: 'bolder', color: 'white'}}>Review local parks in your area</p>
+       <h1 style={{fontSize: '130px', color: 'white'}}>Park Rangers</h1> 
+        <p style={{fontSize: '30px', fontWeight: 'bolder', color: 'white'}}>Review local parks in your area</p>
         <button onClick={onClick} className="btn btn-lg button mr-3 btn-warning">See parks by age</button>
         <button onClick={onClick} className="btn btn-lg button mr-3 btn-warning">See parks by area</button>
         <button onClick={onClick} className="btn btn-lg button mr-3 btn-warning">See parks by popularity</button>
