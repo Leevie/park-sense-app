b0d2c25e9673949d46fa7197533f5e09e9db75e7
Leevie Martin <leevie@gmail.com>
2019-07-08 19:43:20 -0500

    Created Modal, updated some styling and layout

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,6 +7,13 @@ function NavBar() {
   return (
     <div className="container">
     <ul className="nav navBar justify-content-end">
+      <li>
+      <a class="navbar-brand" href="#">
+    <img src="/docs/4.2/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""/>
+    Bootstrap
+  </a>
+
+      </li>
       <li className="nav-item">
         <Link to="/" role="button" className="btn btn-link">
         <span className= "nav-span" style={{color:window.location.pathname === "/" ? "green" : "white"}}>Home</span> 
@@ -27,6 +34,12 @@ function NavBar() {
       <span className= "nav-span" style={{ color: window.location.pathname === "/signup" ? "green" : "white"}}>Sign Up</span>  
       </Link>
       </li>
+      <li className="nav-item">
+      <Link to="/review" role="button" className="btn btn-link">
+      <span className= "nav-span" style={{ color: window.location.pathname === "/review" ? "green" : "white"}}>Review</span>  
+      </Link>
+      </li>
+
     </ul>
   </div>
   );
