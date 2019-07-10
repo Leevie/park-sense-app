404d411b353d5d76f2fa42ec85ddda33a1e32b46
Peter W <42481836+peterwra@users.noreply.github.com>
2019-07-09 22:29:50 -0500

    the build for auth

---
+++ client/build/precache-manifest.0e82dc39eb323c3a6768cb205841536c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1f6d348caeb11c016991cbe7ff9bad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735bc1cc15c9357bc36d",
+    "url": "/static/css/main.a800378d.chunk.css"
+  },
+  {
+    "revision": "cb3a783f9a4b824704da",
+    "url": "/static/js/2.044d7696.chunk.js"
+  },
+  {
+    "revision": "735bc1cc15c9357bc36d",
+    "url": "/static/js/main.ab0517b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
